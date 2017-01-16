314d28641098d8342dee1b0521c318078f03f4bb
James Hall <james.hall@mulesoft.com>
2017-01-16 14:32:06 -0800

    adding new stuff

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
 // Use to modify name and question size to match parent div
 
-// (function($) {
-//     $.fn.textfill = function(maxFontSize) {
-//         maxFontSize = parseInt(maxFontSize, 10);
-//         return this.each(function(){
-//             var ourText = $("span", this),
-//                 parent = ourText.parent(),
-//                 maxHeight = parent.height(),
-//                 maxWidth = parent.width(),
-//                 fontSize = parseInt(ourText.css("fontSize"), 10),
-//                 multiplier = maxWidth/ourText.width(),
-//                 newSize = (fontSize*(multiplier-0.1));
-//             ourText.css(
-//                 "fontSize", 
-//                 (maxFontSize > 0 && newSize > maxFontSize) ? 
-//                     maxFontSize : 
-//                     newSize
-//             );
-//         });
-//     };
-// })(jQuery);
-
 //Psuedocode
 //Startup, render the intro and button
 
@@ -58,22 +37,99 @@ firebase.initializeApp(config);
 // connect your Firebase application using your reference URL
 var cardAppReference = firebase.database();
 
+function writeUserData(name, imageUrl) {
+  cardAppReference.ref('people/' + name).set({
+    name: name,
+    profile_picture : imageUrl
+  });
+}
+
 $(document).ready(function() {
     // Listen for event, perform API call on click
     $("#generateButton").on('click', function(event) {
         $.get( "https://anypoint.mulesoft.com/apiplatform/proxy/https://mocksvc.mulesoft.com/mocks/a7f646cf-2da6-4263-8bd4-5e063563c66e/photos?client_id=123&client_secret=123", function( data ) {
             $( ".result" ).html( data );
-            alert( "Load was performed." );
-            console.log(data);
-    //*write API response to firebase
+
             // create a section for photos in db
             var cardReference = cardAppReference.ref('photos')
+            // write response to photos section in db
+            cardReference.set(data);
+            // $(".showMe").append(data);
+            $("body").removeClass('cardsShown');
+            $("h1").removeClass('mainTitle');
+            $("button").html('Refresh');
         });
     });
+    //Get content for cards, add cards to DOM
+    photoClass.getPhotos();
 });
 
+var photoClass = {
+    getPhotos() {
+        //retrieve photos 
+        cardAppReference.ref('photos').on('value', function (results) {
+            returnObject = results.val();
+            console.log(returnObject.totalResults);
+            
+
+            if (returnObject.totalResults > 40) {
+                console.log("more than 40")
+            for(var i = 0; i < returnObject.totalResults; i++) {
+                
+                var obj = returnObject.Resources[i];
+
+                markup =
+                    `<div class="cardBorder">
+                        <div class="container">
+                          <div class="image">
+                            <img src="${obj.url}"/>
+                          </div>
+                          <div class="name">
+                            <h1>${obj.preferedName}</h1> 
+                          </div>
+                          <div class="question">
+                            <h3>What are your top three favorite books of all time?</h3>
+                          </div>
+                          <div class="answer">
+                          </div>
+                        </div>
+                      </div>`;
+                document.body.innerHTML = markup;
 
 
+            };                
+            } else {
+                console.log("less than 40");
+                console.log("printing +" + returnObject.totalResults)
+                for(var i = 0; i < returnObject.totalResults; i++) {
+ 
+                    var obj = returnObject.Resources[i];
+
+                    var markup =
+                        `<div class="cardBorder">
+                            <div class="container">
+                              <div class="image">
+                                <img class="portrait" src="${obj.url}"/>
+                              </div>
+                              <div class="name">
+                                ${obj.preferedName}
+                              </div>
+                              <div class="question">
+                                What are your top three favorite books of all time?
+                              </div>
+                              <div class="answer">
+                              </div>
+                            </div>
+                          </div>`;
+                    $("#insertCards").append(markup);
+                }
+                
+            }
+        })
+    }
+}
+
+// Create function for portrait vs landscape resizing (max height )
 
 //*query number of people in response object
 
